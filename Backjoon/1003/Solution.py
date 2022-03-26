ac14774fede150c69958b5fbbe4d087fdb79c2b1
jeongmin <lidiawjdals@gmail.com>
2022-03-26 15:07:02 +0900

    init

---
+++ Backjoon/1003/Solution.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.stdin = open(r'C:\Users\lidia\OneDrive\바탕 화면\programming\github\algorithm_test\Backjoon\1003\input.txt','r')
+
+a = int(input())
+list = [1,2,3]
+
+if a not in list :
+    print(a)
